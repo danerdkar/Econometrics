2605d49d9c30a8412f177f843dbd22d469702d18
danerdkar <102828169+danerdkar@users.noreply.github.com>
2022-04-20 10:46:54 -0500

    Add files via upload

---
+++ Pset 2 Problem 5c.R
@@ -0,0 +1,17 @@
+mu.college
+length <- length(yrs_educ)
+
+var <- 0 
+
+for (x in 1:length(yrs_educ)) {
+  if (yrs_educ[x] == 16){
+    var <- var + (wkly_wage[y] - mu.college)^2
+  }
+}
+
+#Based on 4(j)
+
+n <- 329509
+variance=var/n
+se_college=sqrt(variance/phat)/sqrt(n)
+se_college
